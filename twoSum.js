1c1477e49446f5a9c8e292c5fc729ee02b20ec44
fiez alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-05-22 12:43:01 +0300

    change in file twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -4,17 +4,29 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  // =============== One solution ============== o(n^2)
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+  // return [];
+
+  // ================ Two solution ============ o(n)
+  obj = {};
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+    const match = target - nums[i];
+    if (match in obj) {
+      console.log([i, obj[match]]);
+      return [obj[match] , i];
     }
+    obj[nums[i]] = i;
   }
-  return [];
 };
 
-twoSum([2, 15, 11,7], 9);
+twoSum([2, 15, 11, 7], 9);
 twoSum([3, 2, 4], 6);
 twoSum([3, 3], 6);
 twoSum([6, 7, 89, 9], 12);
