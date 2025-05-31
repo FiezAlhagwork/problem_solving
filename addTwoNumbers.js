23a32430913ecf724938b188304d261f7d1e3c1d
fiez alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-05-31 22:07:10 +0300

    first commit

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -10,6 +10,21 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
+const  addTwoNumbers = function(l1, l2) {
+    
+
+    let sumArray = Number(l1.reverse().join('')) + Number(l2.reverse().join(''))
+    let l3 = sumArray.toString().split('').map(Number).reverse()
+    if(l3[0] === 0 ) {
+        console.log("not found");
+        return [0]
+    }
+    console.log(l3)
+    return l3
+     
+
+
     
 };
+
+addTwoNumbers([0], [0])
