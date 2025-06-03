eb245e283d16d89281d4cbbf7a75892415fcefe1
Fiez Alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-06-03 13:18:39 +0300

    edit file merge string alternately

--- mergeStringsAlternately.js
+++ mergeStringsAlternately.js
@@ -5,17 +5,15 @@
  */
 const mergeAlternately = function (word1, word2) {
   let merged = "";
-  let i = 0;
-  let j = 0;
-                          
+  let i = 0,
+    j = 0;
+
   while (i < word1.length || j < word2.length) {
     if (i < word1.length) {
-      merged += word1[i];
-      i++;
+      merged += word1[i++];
     }
     if (j < word2.length) {
-      merged += word2[j];
-      j++;
+      merged += word2[j++];
     }
   }
 
